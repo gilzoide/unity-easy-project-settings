9e91d62f7f535f6867fb72a639a087e9c2d640de
gilzoide <gilzoide@gmail.com>
2022-09-11 18:24:23 -0300

    Add script field in error settings provider if found
    This makes it easier to find/edit the faulty script

--- Editor/ErrorProjectSettingsProvider.cs
+++ Editor/ErrorProjectSettingsProvider.cs
@@ -1,20 +1,38 @@
 using System;
+using System.Linq;
 using UnityEditor;
 
 namespace Gilzoide.EasyProjectSettings.Editor
 {
     public class ErrorProjectSettingsProvider : SettingsProvider
     {
+        private MonoScript _script;
         private Exception _exception;
 
         public ErrorProjectSettingsProvider(Type type, Exception exception) : base(ProjectSettingsProviderGroup.GetSettingsPath(type), SettingsScope.Project)
         {
             _exception = exception;
+            _script = GetScriptForType(type);
         }
 
         public override void OnGUI(string searchContext)
         {
             EditorGUILayout.HelpBox(_exception.Message, MessageType.Error);
+            if (_script)
+            {
+                using (new EditorGUI.DisabledScope(true))
+                {
+                    EditorGUILayout.ObjectField("Script", _script, typeof(MonoScript), false);
+                }
+            }
+        }
+
+        private static MonoScript GetScriptForType(Type type)
+        {
+            return AssetDatabase.FindAssets($"t:MonoScript {type.Name}")
+                .Select(AssetDatabase.GUIDToAssetPath)
+                .Select(AssetDatabase.LoadAssetAtPath<MonoScript>)
+                .FirstOrDefault();
         }
     }
 }
