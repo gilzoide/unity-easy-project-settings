e2cdd603b10e560ae3d1959f3d0e77cacb9fd17a
gilzoide <gilzoide@gmail.com>
2022-09-17 08:58:58 -0300

    Add documentation comments, reorganize code

--- Runtime/ProjectSettings.cs
+++ Runtime/ProjectSettings.cs
@@ -10,20 +10,72 @@ namespace Gilzoide.EasyProjectSettings
 {
     public static class ProjectSettings
     {
+        /// <summary>
+        /// Load the project settings of type <paramref name="type"/>.
+        /// <para>
+        /// Assets will not be loaded if not present in the path specified by the type's <see cref="ProjectSettingsAttribute.AssetPath"/>.
+        /// They will also fail to load on built players if <see cref="ProjectSettingsAttribute.AssetPath"/> is not relative to a Resources folder.
+        /// </para>
+        /// </summary>
+        /// <returns>
+        /// The loaded project settings or <see langword="null"/> if loading failed.
+        /// </returns>
+        /// <param name="type"><see cref="ScriptableObject"/> subclass. Must have a <see cref="ProjectSettingsAttribute"/></param>
+        /// <exception cref="ProjectSettingsException">Thrown when <paramref name="type"/> does not have a <see cref="ProjectSettingsAttribute"/></exception>
+        public static ScriptableObject Load(Type type)
+        {
+            ProjectSettingsAttribute attribute = GetAttribute(type);
+#if UNITY_EDITOR
+            return (ScriptableObject) (attribute.IsRelativeToAssets
+                ? AssetDatabase.LoadAssetAtPath(attribute.AssetPath, type)
+                : UnityEditorInternal.InternalEditorUtility.LoadSerializedFileAndForget(attribute.AssetPath).FirstOrDefault());
+#else
+            return Resources.Load<ScriptableObject>(attribute.ResourcesPath);
+#endif
+        }
+        /// <summary>Typed version of <see cref="Load"/></summary>
         public static T Load<T>() where T : ScriptableObject
         {
             return (T) Load(typeof(T));
         }
-        public static ScriptableObject Load(Type type)
+
+        /// <summary>
+        /// Try loading the project settings of type <paramref name="type"/> into <paramref name="settings"/>.
+        /// <para>
+        /// Assets will not be loaded if not present in the path specified by the type's <see cref="ProjectSettingsAttribute.AssetPath"/>.
+        /// They will also fail to load on built players if <see cref="ProjectSettingsAttribute.AssetPath"/> is not relative to a Resources folder.
+        /// </para>
+        /// </summary>
+        /// <returns>
+        /// If the asset was succesfully loaded, <paramref name="settings"/> will contain the loaded asset and <see langword="true"/> will be returned.
+        /// On failure, <paramref name="settings"/> will be <see langword="null"/> and <see langword="false"/> will be returned.
+        /// </returns>
+        /// <param name="type"><see cref="ScriptableObject"/> subclass. Must have a <see cref="ProjectSettingsAttribute"/></param>
+        /// <exception cref="ProjectSettingsException">Thrown when <paramref name="type"/> does not have a <see cref="ProjectSettingsAttribute"/></exception>
+        public static bool TryLoad(Type type, out ScriptableObject settings)
         {
-            TryLoad(type, out ScriptableObject settingsObject);
-            return settingsObject;
+            settings = Load(type);
+            return settings != null;
+        }
+        /// <summary>Typed version of <see cref="TryLoad"/></summary>
+        public static bool TryLoad<T>(out T settings) where T : ScriptableObject
+        {
+            settings = Load<T>();
+            return settings != null;
         }
 
-        public static T LoadOrCreate<T>() where T : ScriptableObject
-        {
-            return (T) LoadOrCreate(typeof(T));
-        }
+        /// <summary>
+        /// Load the project settings of type <paramref name="type"/> or create a new instance if asset could not be loaded.
+        /// <para>
+        /// When running in the editor, new assets are created in disk if loading failed.
+        /// </para>
+        /// </summary>
+        /// <returns>
+        /// The loaded or created project settings.
+        /// See <see cref="Load"/> for details on asset loading.
+        /// </returns>
+        /// <param name="type"><see cref="ScriptableObject"/> subclass. Must have a <see cref="ProjectSettingsAttribute"/></param>
+        /// <exception cref="ProjectSettingsException">Thrown when <paramref name="type"/> does not have a <see cref="ProjectSettingsAttribute"/></exception>
         public static ScriptableObject LoadOrCreate(Type type)
         {
             if (!TryLoad(type, out ScriptableObject settingsObject))
@@ -35,26 +87,14 @@ namespace Gilzoide.EasyProjectSettings
             }
             return settingsObject;
         }
-
-        public static bool TryLoad<T>(out T settings) where T : ScriptableObject
+        /// <summary>Typed version of <see cref="LoadOrCreate"/></summary>
+        public static T LoadOrCreate<T>() where T : ScriptableObject
         {
-            bool result = TryLoad(typeof(T), out ScriptableObject settingsObject);
-            settings = result ? (T) settingsObject : default;
-            return result;
-        }
-        public static bool TryLoad(Type type, out ScriptableObject settings)
-        {
-            ProjectSettingsAttribute attribute = GetAttribute(type);
-#if UNITY_EDITOR
-            settings = (ScriptableObject) (attribute.IsRelativeToAssets
-                ? AssetDatabase.LoadAssetAtPath(attribute.AssetPath, type)
-                : UnityEditorInternal.InternalEditorUtility.LoadSerializedFileAndForget(attribute.AssetPath).FirstOrDefault());
-#else
-            settings = Resources.Load<ScriptableObject>(attribute.ResourcesPath);
-#endif
-            return settings != null;
+            return (T) LoadOrCreate(typeof(T));
         }
 
+        /// <summary>Get the <see cref="ProjectSettingsAttribute"/> from <paramref name="type"/></summary>
+        /// <exception cref="ProjectSettingsException">Thrown when <paramref name="type"/> does not have a <see cref="ProjectSettingsAttribute"/></exception>
         public static ProjectSettingsAttribute GetAttribute(Type type)
         {
             ProjectSettingsAttribute attribute = type.GetCustomAttribute<ProjectSettingsAttribute>();
--- Runtime/ProjectSettingsAttribute.cs
+++ Runtime/ProjectSettingsAttribute.cs
@@ -6,20 +6,40 @@ namespace Gilzoide.EasyProjectSettings
     [AttributeUsage(AttributeTargets.Class)]
     public class ProjectSettingsAttribute : Attribute
     {
+        public const string AssetsDirectoryIdentifier = "Assets/";
         public const string ResourcesDirectoryIdentifier = "/Resources/";
 
+        /// <summary>
+        /// Path where project settings asset will be loaded from/saved to.
+        /// Since project settings must be ScriptableObjects, the setter forces a ".asset" extension.
+        /// <para>
+        /// If the given path is relative to a "Resources" folder, the asset will be loadable by
+        /// code in built players by using <see cref="ProjectSettings.Load"/>
+        /// </para>
+        /// </summary>
         public string AssetPath
         {
             get => _assetPath;
             set => _assetPath = Path.ChangeExtension(value, "asset");
         }
+        private string _assetPath;
+        /// <summary>
+        /// Path used to place the SettingsProvider in the tree view of the Settings window.
+        /// The path should be unique among all other settings paths and should use "/" as its separator.
+        /// </summary>
         public string SettingsPath { get; set; }
+        /// <summary>Display name of the SettingsProvider as it appears in the Settings window</summary>
         public string Label { get; set; }
 
-        private string _assetPath;
-
-        public bool IsRelativeToAssets => AssetPath.StartsWith("Assets/");
+        /// <summary>Whether <see cref="AssetPath"/> is relative to the "Assets" folder</summary>
+        public bool IsRelativeToAssets => AssetPath.StartsWith(AssetsDirectoryIdentifier);
+        /// <summary>Whether <see cref="AssetPath"/> is relative to a "Resources" folder</summary>
         public bool IsRelativeToResources => AssetPath.IndexOf(ResourcesDirectoryIdentifier) >= 0;
+        /// <summary>
+        /// If <see cref="AssetPath"/> is relative to a "Resources" folder, gets the relative path for loading the
+        /// asset with <a href="https://docs.unity3d.com/ScriptReference/Resources.Load.html">Resources.Load</a>.
+        /// Returns <see langword="null"/> otherwise.
+        /// </summary>
         public string ResourcesPath
         {
             get
